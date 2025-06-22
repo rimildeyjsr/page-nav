684a642302441a0ce44be7e901d6cc7ee68a7867
Rimil Dey <rimildeyjsr@gmail.com>
2025-06-22 19:58:39 +0530

    added divider

---
+++ app/components/PageNavigation/Divider.tsx
@@ -0,0 +1,7 @@
+export const Divider = () => {
+  return (
+    <div className="flex items-center justify-center w-10 h-full">
+      <div className="w-full border-t border-dotted border-gray-300"></div>
+    </div>
+  );
+};
